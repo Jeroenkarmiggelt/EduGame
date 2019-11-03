a8c2877d9742d414889446a8fff425e1b78fc906
Jeroenkarmiggelt <33348622+Jeroenkarmiggelt@users.noreply.github.com>
2019-11-03 20:39:30 +0100

    [TASK] Added scoring system & GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+   // public GameObject hazard;
+   // public Vector3 spawnValues;
+   // public int hazardCount;
+    public float winScore;
+   // public float startWait;
+   // public float waveWait;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+    //    score = 0;
+        UpdateScore();
+     //   StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+   public void SpawnWaves()
+    {
+ 
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+         //       break;
+            }
+ 
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+        if (score == winScore)
+        {
+            GameOver();
+        }
+
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverText.text = "Game Over!";
+        restartText.text = "Press 'R' for Restart";
+        restart = true;
+        gameOver = true;
+    }
+}
--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -6,18 +6,33 @@ using UnityEngine.UI;
 public class ScoreCounter : MonoBehaviour
 {
 
-    public Text countText;
-    public Text winText;
+  //  public Text countText;
+  //  public Text winText;
 
     private Rigidbody rb;
-    private int count;
+  //  private int count;
+
+    private GameController gameController;
+    public int scoreValue;
+    public int junkValue;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
-        winText.text = "";
+    //    count = 0;
+    //    SetCountText();
+    //    winText.text = "";
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+
     }
 
     void FixedUpdate()
@@ -30,18 +45,26 @@ public class ScoreCounter : MonoBehaviour
        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
+            gameController.AddScore(scoreValue);
+            //          count = count + 1;
+            //          SetCountText();
         }
-                    
-    }
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 4)
+        if (other.gameObject.CompareTag("Junk"))
         {
-            winText.text = "You win";
+            other.gameObject.SetActive(false);
+            gameController.AddScore(junkValue);
+            //          count = count + 1;
+  //          SetCountText();
         }
+
     }
+  //  void SetCountText()
+  //  {
+   //     countText.text = "Count: " + count.ToString();
+    //    if (count >= 4)
+    //    {
+    //        winText.text = "You win";
+    //    }
+   // }
 
 }
